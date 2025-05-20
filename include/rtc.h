4030c61b9eec8db95d22a55e11a0c0c462e6480e
Gabriel Germano <gabriel.germano@edge.ufal.br>
2025-05-20 14:55:06 -0300

    feat: adds the sntp thread to the project
    Signed-off-by: Gabriel Germano <gabriel.germano@edge.ufal.br>

--- include/rtc.h
+++ include/rtc.h
@@ -1,21 +1,26 @@
+#ifndef RTC_H
+#define RTC_H
+
+#include <zephyr/device.h>
+#include <zephyr/drivers/rtc.h>
 #include <zephyr/kernel.h>
+#include <zephyr/logging/log.h>
+#include <zephyr/net/net_ip.h>
 #include <zephyr/net/sntp.h>
 #include <zephyr/net/socket.h>
-#include <zephyr/drivers/rtc.h>
-#include <zephyr/logging/log.h>
-#include <zephyr/device.h>
 #include <zephyr/settings/settings.h>
-#include <zephyr/net/net_ip.h>
 
-#include <stdint.h>
+#include <time.h>
+
 #include <stdbool.h>
-#include <string.h>
+#include <stdint.h>
 
 /**
- * @brief 
- * 
- * @param time 
- * @return int 
+ * @brief Function to get the current time from SNTP server.
+ *
+ * @param time Pointer to the rtc_time structure to store the retrieved time.
+ * @return int 0 on success, negative error code on failure.
  */
 int tracker_get_time(struct rtc_time *time);
 
+#endif /* RTC_H */
--- src/rtc.c
+++ src/rtc.c
@@ -4,14 +4,25 @@ LOG_MODULE_REGISTER(rtc, LOG_LEVEL_DBG);
 
 #define RTC_DEVICE DEVICE_DT_GET(DT_NODELABEL(rtc))
 
-#define SYS_TIME_SETTINGS_KEY "data"
-
+#define SYS_TIME_SETTINGS_KEY    "data"
 #define SYS_TIME_SETTINGS_PREFIX "sys_time"
+#define TIME_QUEUE_SIZE          5
+
+K_MSGQ_DEFINE(time_msgq, sizeof(struct rtc_time), TIME_QUEUE_SIZE, 4);
 
 static struct sys_time {
 	char server[64];
 	int8_t timezone;
-} self = {.server = "pool.ntp.org", .timezone = 0};
+} self = {.server = CONFIG_DEFAULT_SNTP_SERVER, .timezone = 0};
+
+/**
+ * @brief Function to get the current time from SNTP server.
+ * 
+ * @param p1 Void pointer to the first parameter (not used).
+ * @param p2 Void pointer to the second parameter (not used).
+ * @param p3 Void pointer to the third parameter (not used).
+ */
+static void time_thread_function(void *p1, void *p2, void *p3);
 
 int tracker_get_time(struct rtc_time *time)
 {
@@ -24,7 +35,7 @@ int tracker_get_time(struct rtc_time *time)
 	server_addr.sin_port = htons(123);
 
 	if (strlen(self.server) == 0) {
-		strcpy(self.server, "pool.ntp.org");
+		strcpy(self.server, CONFIG_DEFAULT_SNTP_SERVER);
 
 		err = settings_save_one(SYS_TIME_SETTINGS_PREFIX "/" SYS_TIME_SETTINGS_KEY, &self,
 					sizeof(self));
@@ -52,7 +63,54 @@ int tracker_get_time(struct rtc_time *time)
 
 	sntp_close(&ctx);
 
+	time_t unix_time = datetime.seconds;
+	unix_time += self.timezone * 3600;
+
+	struct tm *tm_info = gmtime(&unix_time);
+
+	if (tm_info == NULL) {
+		LOG_ERR("Failed to convert time to UTC");
+		return -EINVAL;
+	}
+
+	time->tm_sec = tm_info->tm_sec;
+	time->tm_min = tm_info->tm_min;
+	time->tm_hour = tm_info->tm_hour;
+	time->tm_mday = tm_info->tm_mday;
+	time->tm_mon = tm_info->tm_mon;
+	time->tm_year = tm_info->tm_year + 1900;
+	time->tm_wday = tm_info->tm_wday;
+	time->tm_yday = tm_info->tm_yday;
+	time->tm_isdst = tm_info->tm_isdst;
+
+	LOG_DBG("Time: %04d-%02d-%02d %02d:%02d:%02d", time->tm_year, time->tm_mon + 1,
+		time->tm_mday, time->tm_hour, time->tm_min, time->tm_sec);
+
 	return 0;
 }
 
-K_THREAD_DEFINE(rtc_thread, 1024, tracker_get_time, NULL, NULL, NULL, 3, 0, 0);
+static void time_thread_function(void *p1, void *p2, void *p3)
+{
+    struct rtc_time time;
+    int err;
+    
+    while (1) {
+        err = tracker_get_time(&time);
+        if (err) {
+            LOG_ERR("Failed to get time from SNTP: %d", err);
+        } else {
+            err = k_msgq_put(&time_msgq, &time, K_NO_WAIT);
+            if (err) {
+                LOG_WRN("Time queue full, message not sent");
+            } else {
+                LOG_INF("Time sent to main thread: %04d-%02d-%02d %02d:%02d:%02d", 
+                    time.tm_year, time.tm_mon + 1, time.tm_mday, 
+                    time.tm_hour, time.tm_min, time.tm_sec);
+            }
+        }
+        
+        k_sleep(K_MINUTES(1));
+    }
+}
+
+K_THREAD_DEFINE(time_thread, 1024, time_thread_function, NULL, NULL, NULL, 3, 0, 0);
